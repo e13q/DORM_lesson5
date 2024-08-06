25f4bb335872916cb0c35ee35ca7b8f0f3def8e3
e13q <110967581+e13q@users.noreply.github.com>
2024-08-07 00:11:25 +0300

    add field Flat.owner_pure_phone

--- property/models.py
+++ property/models.py
@@ -1,10 +1,14 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from phonenumber_field.modelfields import PhoneNumberField
 
 
 class Flat(models.Model):
     owner = models.CharField('ФИО владельца', max_length=200)
+    owner_pure_phone = PhoneNumberField(
+        verbose_name='Нормализованный номер владельца', blank=True, null=True
+    )
     owners_phonenumber = models.CharField('Номер владельца', max_length=20)
     new_building = models.BooleanField('Новостройка', blank=True, null=True)
     created_at = models.DateTimeField(
@@ -42,7 +46,8 @@ class Flat(models.Model):
 
     has_balcony = models.NullBooleanField('Наличие балкона', db_index=True)
     active = models.BooleanField('Активно-ли объявление', db_index=True)
-    who_liked = models.ManyToManyField(User, verbose_name='Кто лайкнул')
+    who_liked = models.ManyToManyField(
+        User, verbose_name='Кто лайкнул', blank=True, null=True)
     construction_year = models.IntegerField(
         'Год постройки здания',
         null=True,
