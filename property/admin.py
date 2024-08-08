9f83951d5bbe674d3e38dbe15cac8fbfd59579a0
e13q <110967581+e13q@users.noreply.github.com>
2024-08-08 23:29:39 +0300

    show owners for flats in admin panel

--- property/admin.py
+++ property/admin.py
@@ -3,6 +3,11 @@ from django.contrib import admin
 from .models import Flat, Complaint, Owner
 
 
+class OwnerFlatsInline(admin.TabularInline):
+    model = Owner.flats.through
+    raw_id_fields = ('owner',)
+
+
 class FlatAdmin(admin.ModelAdmin):
     search_fields = ('address', 'town_district', 'town')
     readonly_fields = ['created_at']
@@ -10,6 +15,9 @@ class FlatAdmin(admin.ModelAdmin):
     list_editable = ('new_building', )
     list_filter = ('new_building', 'rooms_number', 'has_balcony')
     raw_id_fields = ('who_liked',)
+    inlines = [
+        OwnerFlatsInline,
+    ]
 
 
 class ComplaintAdmin(admin.ModelAdmin):
