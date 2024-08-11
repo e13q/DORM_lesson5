2f11b9a55a323fe2270dc9337c84679d30c156c4
e13q <110967581+e13q@users.noreply.github.com>
2024-08-11 22:54:14 +0300

    decorator @register use

--- property/admin.py
+++ property/admin.py
@@ -8,6 +8,7 @@ class OwnerFlatsInline(admin.TabularInline):
     raw_id_fields = ('owner',)
 
 
+@admin.register(Flat)
 class FlatAdmin(admin.ModelAdmin):
     search_fields = ('address', 'town_district', 'town')
     readonly_fields = ['created_at']
@@ -20,14 +21,11 @@ class FlatAdmin(admin.ModelAdmin):
     ]
 
 
+@admin.register(Complaint)
 class ComplaintAdmin(admin.ModelAdmin):
     raw_id_fields = ('user', 'flat')
 
 
+@admin.register(Owner)
 class OwnerAdmin(admin.ModelAdmin):
     raw_id_fields = ('flats',)
-
-
-admin.site.register(Flat, FlatAdmin)
-admin.site.register(Complaint, ComplaintAdmin)
-admin.site.register(Owner, OwnerAdmin)
